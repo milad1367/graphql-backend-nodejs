950e07b1ccb1c19d70c6482a958b4723bdbb69f0
milad asghari <miladasghari2016@gmail.com>
2019-10-27 14:41:28 +0330

    change data

--- src/data.ts
+++ src/data.ts
@@ -1,42 +1,45 @@
+
 const data = 
+
+
 [
-    {'id': 1,"name":"Nicole Fowlston","image":"http://dummyimage.com/500x500.jpg/5fa2dd/ffffff","content":"orci eget orci vehicula condimentum curabitur in libero ut massa volutpat",
+    {'id': 1,"name":"Nicole Fowlston","image":"https://source.unsplash.com/500x500/?city","content":"orci eget orci vehicula condimentum curabitur in libero ut massa volutpat",
     "comments":
         [{'id': 1,"name":"Giffer Penhaligon","content":"non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla"},
         {'id': 2,"name":" pedram","content":"non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla"},
         {'id': 3,"name":"rasel","content":"non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla"},
         {'id': 4,"name":"androw","content":"non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla"},
         ]},
-    {"id":2,"name":"Nowell MacElroy","image":"http://dummyimage.com/500x500.jpg/cc0000/ffffff","content":"quis odio consequat varius integer ac leo pellentesque ultrices mattis odio donec vitae nisi nam ultrices libero non mattis",
+    {"id":2,"name":"Nowell MacElroy","image":"https://source.unsplash.com/random/500×500/?city","content":"quis odio consequat varius integer ac leo pellentesque ultrices mattis odio donec vitae nisi nam ultrices libero non mattis",
     "comments":
-      [{"id":1,"name":"Giffer Penhaligon","content":"non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla"},
-      {"id":2,"name":"Traci Nassie","content":"tortor duis mattis egestas metus aenean fermentum donec ut mauris"},
-      {"id":3,"name":"Linnea Lapidus","content":"vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam"},
-      {"id":4,"name":"Luis Deane","content":"morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede"},
-      {"id":5,"name":"Bobinette Medway","content":"tellus in sagittis dui vel nisl duis ac nibh fusce lacus"}
+      [{"id":1,"name":"Giffer Penhaligon","content":"non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":2,"name":"Traci Nassie","content":"tortor duis mattis egestas metus aenean fermentum donec ut mauris","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":3,"name":"Linnea Lapidus","content":"vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":4,"name":"Luis Deane","content":"morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":5,"name":"Bobinette Medway","content":"tellus in sagittis dui vel nisl duis ac nibh fusce lacus","image":"https://source.unsplash.com/random/500*500/?fruit"}
       ]},
-    {"id":3,"name":"Jacki Burrus","image":"http://dummyimage.com/500x500.jpg/ff4444/ffffff","content":"donec ut dolor morbi vel lectus in quam fringilla rhoncus mauris enim leo",
+    {"id":3,"name":"Jacki Burrus","image":"https://source.unsplash.com/random/500*500/?city,night","content":"donec ut dolor morbi vel lectus in quam fringilla rhoncus mauris enim leo",
     "comments":
-      [{"id":1,"name":"Giffer Penhaligon","content":"non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla"},
-      {"id":2,"name":"Traci Nassie","content":"tortor duis mattis egestas metus aenean fermentum donec ut mauris"},
-      {"id":3,"name":"Linnea Lapidus","content":"vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam"},
-      {"id":4,"name":"Luis Deane","content":"morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede"},
-      {"id":5,"name":"Bobinette Medway","content":"tellus in sagittis dui vel nisl duis ac nibh fusce lacus"}
+      [{"id":1,"name":"Giffer Penhaligon","content":"non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":2,"name":"Traci Nassie","content":"tortor duis mattis egestas metus aenean fermentum donec ut mauris","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":3,"name":"Linnea Lapidus","content":"vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":4,"name":"Luis Deane","content":"morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":5,"name":"Bobinette Medway","content":"tellus in sagittis dui vel nisl duis ac nibh fusce lacus","image":"https://source.unsplash.com/random/500*500/?fruit"}
       ]},
-    {"id":4,"name":"Erda Ranaghan","image":"http://dummyimage.com/500x500.jpg/dddddd/000000","content":"elit proin interdum mauris non ligula pellentesque ultrices phasellus id",
+    {"id":4,"name":"Erda Ranaghan","image":"https://source.unsplash.com/random/500*500/?fruit","content":"elit proin interdum mauris non ligula pellentesque ultrices phasellus id",
     "comments":
-      [{"id":1,"name":"Giffer Penhaligon","content":"non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla"},
-      {"id":2,"name":"Traci Nassie","content":"tortor duis mattis egestas metus aenean fermentum donec ut mauris"},
-      {"id":3,"name":"Linnea Lapidus","content":"vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam"},
-      {"id":4,"name":"Luis Deane","content":"morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede"},
-      {"id":5,"name":"Bobinette Medway","content":"tellus in sagittis dui vel nisl duis ac nibh fusce lacus"}
+      [{"id":1,"name":"Giffer Penhaligon","content":"non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":2,"name":"Traci Nassie","content":"tortor duis mattis egestas metus aenean fermentum donec ut mauris","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":3,"name":"Linnea Lapidus","content":"vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":4,"name":"Luis Deane","content":"morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede","image":"https://source.unsplash.com/random/500*500/?fruit"},
+      {"id":5,"name":"Bobinette Medway","content":"tellus in sagittis dui vel nisl duis ac nibh fusce lacus","image":"https://source.unsplash.com/random/500*500/?fruit"}
       ]},
-    {"id":5,"name":"Dirk Bidnall","image":"http://dummyimage.com/500x500.jpg/cc0000/ffffff","content":"cursus urna ut tellus nulla ut erat id mauris vulputate elementum nullam varius nulla facilisi cras","comments":[]},
-    {"id":6,"name":"Pamela MacCorley","image":"http://dummyimage.com/500x500.jpg/5fa2dd/ffffff","content":"pede lobortis ligula sit amet eleifend pede libero quis orci nullam","comments":[]},
-    {"id":7,"name":"Wenona Dunsford","image":"http://dummyimage.com/500x500.jpg/dddddd/000000","content":"augue vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere","comments":[]},
-    {"id":8,"name":"Florian McGow","image":"http://dummyimage.com/500x500.jpg/ff4444/ffffff","content":"lorem ipsum dolor sit amet consectetuer adipiscing elit proin risus praesent lectus vestibulum quam sapien varius","comments":[]},
-    {"id":9,"name":"Suzanna Berntsson","image":"http://dummyimage.com/500x500.jpg/cc0000/ffffff","content":"at ipsum ac tellus semper interdum mauris ullamcorper purus sit amet nulla quisque arcu libero rutrum ac lobortis vel","comments":[]},
-    {"id":10,"name":"Friederike Poter","image":"http://dummyimage.com/500x500.jpg/cc0000/ffffff","content":"aliquet ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non mi integer ac neque duis bibendum","comments":[]},
+    {"id":5,"name":"Dirk Bidnall","image":"https://source.unsplash.com/random/500*500/?fruit","content":"cursus urna ut tellus nulla ut erat id mauris vulputate elementum nullam varius nulla facilisi cras","comments":[]},
+    {"id":6,"name":"Pamela MacCorley","image":"https://source.unsplash.com/random/500*500/?fruit","content":"pede lobortis ligula sit amet eleifend pede libero quis orci nullam","comments":[]},
+    {"id":7,"name":"Wenona Dunsford","image":"https://source.unsplash.com/random/500*500/?fruit","content":"augue vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere","comments":[]},
+    {"id":8,"name":"Florian McGow","image":"https://source.unsplash.com/random/500*500/?fruit","content":"lorem ipsum dolor sit amet consectetuer adipiscing elit proin risus praesent lectus vestibulum quam sapien varius","comments":[]},
+    {"id":9,"name":"Suzanna Berntsson","image":"https://source.unsplash.com/random/500*500/?fruit","content":"at ipsum ac tellus semper interdum mauris ullamcorper purus sit amet nulla quisque arcu libero rutrum ac lobortis vel","comments":[]},
+    {"id":10,"name":"Friederike Poter","image":"https://source.unsplash.com/random/500*500/?fruit","content":"aliquet ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non mi integer ac neque duis bibendum","comments":[]},
 
 ];
 export default data
